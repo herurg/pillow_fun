8dcc8251807642d9d07e9c33e0df5de52fb9fe9b
herurg <getmanoff@gmail.com>
2024-10-10 10:56:43 +0700

    add player sprite

--- core/game.py
+++ core/game.py
@@ -14,10 +14,11 @@ except:
     serial_on = False
 
 BLACK = (0, 0, 0)
+BG_COLOR = (15, 15, 15)
 
 game_folder = os.path.dirname(__file__)
 
-FPS = 20
+FPS = 10
 clock = pg.time.Clock()
 ico = pg.image.load(game_folder+'./images/ico.png')
 WIDTH = 800
@@ -29,19 +30,30 @@ pg.display.set_icon(ico)
 pg.display.set_caption('Pillow FUN v1')
 running = True
 
+cur_f = 1
 
 class Player(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((50,50))
-        self.image.fill('GREEN')
+        
+        self.image = pg.image.load(game_folder+f'./images/c1_f{cur_f}.png')
+        
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, HEIGHT/2)
+        w, h = pg.display.get_surface().get_size()
+        self.rect.center = (w/2, h/2)
 
     def update(self):
-        self.rect.x +=5
-        if self.rect.left >WIDTH:
-            self.rect.right = 0
+        global cur_f
+        cur_f +=1
+        self.image = pg.image.load(game_folder+f'./images/c1_f{cur_f}.png')
+        self.rect = self.image.get_rect()
+        w, h = pg.display.get_surface().get_size()
+        self.rect.center = (w/2, h/2)
+        if cur_f == 4:
+            cur_f = 1
+        
+        
+
         
         
 all_sprites = pg.sprite.Group()
@@ -51,8 +63,8 @@ all_sprites.add(player)
 
 while running:
     clock.tick(FPS)
-    w, h = pg.display.get_surface().get_size()
-    print(w," ",h)
+    
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
@@ -64,7 +76,7 @@ while running:
     #print (pillow_event)
 
     all_sprites.update()
-    screen.fill(BLACK)
+    screen.fill((0,0,12))
     all_sprites.draw(screen)
     
     pg.display.flip()
